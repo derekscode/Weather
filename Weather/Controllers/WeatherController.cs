39fe8e2f9b96a6cbda11952270f3db101ca9e6d5
Derek <17056283+derekscode@users.noreply.github.com>
2017-12-17 18:37:34 -0800

    it works

---
+++ Weather/Controllers/WeatherController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using Newtonsoft.Json;
+using Weather.Models;
+
+namespace Weather.Controllers
+{
+    [Route("api/[controller]")]
+    public class WeatherController : Controller
+    {
+        [HttpGet("[action]/{city}")]
+        public async Task<IActionResult> City(string city)
+        {
+            using (var client = new HttpClient())
+            {
+                try
+                {
+                    client.BaseAddress = new Uri("http://api.openweathermap.org");
+                    var response = await client.GetAsync($"/data/2.5/weather?q={city}&appid=f17a294f8dfba188ffda2f496a31711f&units=metric");
+                    response.EnsureSuccessStatusCode();
+
+                    var stringResult = await response.Content.ReadAsStringAsync();
+                    var rawWeather = JsonConvert.DeserializeObject<OpenWeatherResponse>(stringResult);
+                    return Ok(new
+                    {
+                        Temp = rawWeather.Main.Temp,
+                        Summary = string.Join(",", rawWeather.Weather.Select(x => x.Main)),
+                        City = rawWeather.Name
+                    });
+                }
+                catch (HttpRequestException httpRequestException)
+                {
+                    return BadRequest($"Error getting weather from OpenWeather: {httpRequestException.Message}");
+                }
+            }
+        }
+
+        //[HttpGet("[action]/{city}")]
+        //public IActionResult City(string city)
+        //{
+        //    return Ok(new { Temp = "12", Summary = "Barmy", City = city });
+        //}
+    }
+}
